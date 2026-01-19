1eb477236239d4f963075cb063ae0b3ce80191a0
effective-range <info@effective-range.com>
2026-01-19 15:37:17 +0100

    fix: reorder ER repo addition in build script

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 # Install latest clang tooling
 set -e -x -o pipefail
 
-# add ER repo
-wget -qO- https://raw.githubusercontent.com/EffectiveRange/infrastructure-configuration/refs/heads/main/aptrepo/apt-server/add_repo.sh | bash
-
 
 # Microchip Tools Require i386 Compatability as Dependency
 dpkg --add-architecture i386 
 apt-get update -yq 
 apt-get install -yq --no-install-recommends build-essential bzip2 cpio curl unzip wget libc6:i386 libx11-6:i386 libxext6:i386 libstdc++6:i386 libexpat1:i386  libxext6 libxrender1 libxtst6 libgtk2.0-0 libxslt1.1 libncurses5-dev gcc python3 python3-pip python3.11-venv inetutils-ping openssh-client pkg-config dpkg-dev nano git sudo gnupg lsb-release software-properties-common procps libusb-1.0-0-dev file less gdb
+
+# add ER repo
+wget -qO- https://raw.githubusercontent.com/EffectiveRange/infrastructure-configuration/refs/heads/main/aptrepo/apt-server/add_repo.sh | bash
 
 # install packaging tools
 . /etc/os-release && \
